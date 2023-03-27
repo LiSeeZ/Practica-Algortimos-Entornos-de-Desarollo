66a046c2b8f8bd63040efd1f3a78fa6265b31d01
LiSeeZ <victxrgxnzalez@gmail.com>
2023-03-27 10:43:39 +0200

    Metodo factorial

--- com.entornos.victorGonzalez.Martin.algoritmos/src/com/entornos/victorGonzalez/Martin/algoritmos/algoritmos.java
+++ com.entornos.victorGonzalez.Martin.algoritmos/src/com/entornos/victorGonzalez/Martin/algoritmos/algoritmos.java
@@ -1,4 +1,5 @@
 package com.entornos.victorGonzalez.Martin.algoritmos;
+
 /**
  * @version 1.0
  * 
@@ -7,13 +8,13 @@ package com.entornos.victorGonzalez.Martin.algoritmos;
 public abstract class algoritmos {
 
 	/**
-	 * Devuelve el numerode fibonacci de dicho numero.
+	 * Recibe un numero entero y devuelve el fibonacci de dicho numero
 	 * 
-	 * @param numero numero al que le haremos el fibonacci 
-	 * 		
-	 * @return	el numero en fibonacci de ese numero.
+	 * @param numero al que le haremos el fibonacci
+	 * 
+	 * @return devuelve el numero fibonacci de dicho numero.
 	 */
-	public int fibonacci (int numero) {
+	public int fibonacci(int numero) {
 		int siguiente = 1, actual = 0, temporal = 0;
 		for (int x = 1; x <= numero; x++) {
 			System.out.print(actual + ", ");
@@ -23,6 +24,21 @@ public abstract class algoritmos {
 		}
 		return actual;
 	}
-	
+
+	/**
+	 * Recibe un numero entero y devuelve el factorial de dicho numero .
+	 * 
+	 * @param numero al que le haremos el factorial
+	 * 
+	 * @return devuelve el factorial de dicho numero .
+	 */
+
+	public double factorial(int numero) {
+		double res = 1;
+		for (int i = 1; i <= numero; i++) {
+			res = res * i;
+		}
+		return res;
+	}
 
 }
