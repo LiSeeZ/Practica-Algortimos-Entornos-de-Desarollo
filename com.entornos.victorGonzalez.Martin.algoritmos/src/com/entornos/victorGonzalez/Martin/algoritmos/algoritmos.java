ebdd67fc4709bb63a8c593a4ecf3a1d9bda944a5
LiSeeZ <victxrgxnzalez@gmail.com>
2023-03-27 11:03:28 +0200

    Metodo primo

--- com.entornos.victorGonzalez.Martin.algoritmos/src/com/entornos/victorGonzalez/Martin/algoritmos/algoritmos.java
+++ com.entornos.victorGonzalez.Martin.algoritmos/src/com/entornos/victorGonzalez/Martin/algoritmos/algoritmos.java
@@ -1,16 +1,16 @@
 package com.entornos.victorGonzalez.Martin.algoritmos;
 
 /**
- * @version 1.0
+ * @version 1.1
  * 
  * @author Victor
  */
 public abstract class algoritmos {
 
 	/**
-	 * Recibe un numero entero y devuelve el fibonacci de dicho numero
+	 * Recibe un numero entero y devuelve el fibonacci de dicho numero.
 	 * 
-	 * @param numero al que le haremos el fibonacci
+	 * @param numero al que le haremos el fibonacci.
 	 * 
 	 * @return devuelve el numero fibonacci de dicho numero.
 	 */
@@ -26,19 +26,38 @@ public abstract class algoritmos {
 	}
 
 	/**
-	 * Recibe un numero entero y devuelve el factorial de dicho numero .
+	 * Recibe un numero entero y devuelve el factorial de dicho numero.
 	 * 
-	 * @param numero al que le haremos el factorial
+	 * @param numero al que le haremos el factorial.
 	 * 
-	 * @return devuelve el factorial de dicho numero .
+	 * @return devuelve el factorial de dicho numero.
 	 */
-
 	public double factorial(int numero) {
 		double res = 1;
 		for (int i = 1; i <= numero; i++) {
 			res = res * i;
 		}
 		return res;
+	}
+
+	/**
+	 * Recibe un numero entero y devuelve true o false en funcion de si es primo o
+	 * no.
+	 * 
+	 * @param numero al que comprobaremos si es primo o no.
+	 * 
+	 * @return devuelve true o false en funcion de si es primo o no.
+	 */
+
+	public static boolean Primo(int numero) {
+		if (numero == 0 || numero == 1 || numero == 4) {
+			return false;
+		}
+		for (int x = 2; x < numero / 2; x++) {
+			if (numero % x == 0)
+				return false;
+		}
+		return true;
 	}
 
 }
