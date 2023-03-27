f7b8e976f042ccf9dff2075fa998583d41050baa
LiSeeZ <victxrgxnzalez@gmail.com>
2023-03-27 10:22:49 +0200

    Metodo fibonacci

--- com.entornos.victorGonzalez.Martin.algoritmos/src/com/entornos/victorGonzalez/Martin/algoritmos/algoritmos.java
+++ com.entornos.victorGonzalez.Martin.algoritmos/src/com/entornos/victorGonzalez/Martin/algoritmos/algoritmos.java
@@ -1,5 +1,28 @@
 package com.entornos.victorGonzalez.Martin.algoritmos;
-
+/**
+ * @version 1.0
+ * 
+ * @author Victor
+ */
 public abstract class algoritmos {
+
+	/**
+	 * Devuelve el numerode fibonacci de dicho numero.
+	 * 
+	 * @param numero numero al que le haremos el fibonacci 
+	 * 		
+	 * @return	el numero en fibonacci de ese numero.
+	 */
+	public int fibonacci (int numero) {
+		int siguiente = 1, actual = 0, temporal = 0;
+		for (int x = 1; x <= numero; x++) {
+			System.out.print(actual + ", ");
+			temporal = actual;
+			actual = siguiente;
+			siguiente = siguiente + temporal;
+		}
+		return actual;
+	}
+	
 
 }
