b51a47ae84edcb2a7f68824e34aafc798d356b87
LiSeeZ <victxrgxnzalez@gmail.com>
2023-04-17 12:24:29 +0200

    Segundo metodo

---
+++ com.entornos.victorGonzalez.Martin.arrays/src/com/entornos/victorGonzalez/arrays/test/testMisArrays.java
@@ -0,0 +1,11 @@
+package com.entornos.victorGonzalez.arrays.test;
+
+import static org.junit.jupiter.api.Assertios.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.entornos.victorGonzalezMartin.arrays.MisArrays;
+
+class testMisArrays {
+
+}
--- com.entornos.victorGonzalez.Martin.arrays/src/com/entornos/victorGonzalezMartin/arrays/MisArrays.java
+++ com.entornos.victorGonzalez.Martin.arrays/src/com/entornos/victorGonzalezMartin/arrays/MisArrays.java
@@ -1,16 +1,19 @@
 package com.entornos.victorGonzalezMartin.arrays;
+
+import java.util.Arrays;
+
 /**
- * @version 1
+ * @version 1.1
  * 
  * @author Victor
  */
 public abstract class MisArrays {
-	
+
 	/**
-	 * Recibe un array de números enteros y devuelve la media con decimales de dicho array.
+	 * Recibe un array de números enteros y devuelve la media con decimales de dicho
+	 * array.
 	 * 
 	 * @param array de números enteros
-	 * .
 	 * 
 	 * @return devuelve la media con decimales de dicho array.
 	 */
@@ -21,6 +24,28 @@ public abstract class MisArrays {
 			sum += array[i];
 		}
 		return sum / array.length;
+	}
+
+	/**
+	 * Recibe un array de números enteros y devuelve la mediana con decimales de
+	 * dicho array.
+	 * 
+	 * @param array de números enteros
+	 * 
+	 * @return devuelve la mediana con decimales de dicho array.
+	 */
+
+	public static double mediana(int[] array) {
+		Arrays.sort(array);
+
+		double mediana = 0.0;
+		int mitad = array.length / 2;
+		if (array.length % 2 == 0) {
+			mediana = (array[mitad - 1] + array[mitad]) / 2;
+		} else {
+			mediana = array[mitad];
+		}
+		return mediana;
 	}
 
 }
