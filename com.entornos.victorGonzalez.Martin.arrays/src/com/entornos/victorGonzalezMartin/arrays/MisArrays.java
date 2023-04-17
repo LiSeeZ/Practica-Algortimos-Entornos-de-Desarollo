aa5cdfe486c983c263756b1c31ce7b61189e3439
LiSeeZ <victxrgxnzalez@gmail.com>
2023-04-17 11:06:44 +0200

    Primer metodo

--- com.entornos.victorGonzalez.Martin.arrays/src/com/entornos/victorGonzalezMartin/arrays/MisArrays.java
+++ com.entornos.victorGonzalez.Martin.arrays/src/com/entornos/victorGonzalezMartin/arrays/MisArrays.java
@@ -1,10 +1,26 @@
 package com.entornos.victorGonzalezMartin.arrays;
-
+/**
+ * @version 1
+ * 
+ * @author Victor
+ */
 public abstract class MisArrays {
+	
+	/**
+	 * Recibe un array de números enteros y devuelve la media con decimales de dicho array.
+	 * 
+	 * @param array de números enteros
+	 * .
+	 * 
+	 * @return devuelve la media con decimales de dicho array.
+	 */
 
-	public static void main(String[] args) {
-		// TODO Apéndice de método generado automáticamente
-
+	public static double media(int[] array) {
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+		return sum / array.length;
 	}
 
 }
